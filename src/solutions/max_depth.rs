1b384a8672c78c3e5d6fe6690b70104b491e425b
Azriel Lector <azriel.liang@gmail.com>
2019-12-10 23:26:53 +0800

    Max depth

--- src/solutions/max_depth.rs
+++ src/solutions/max_depth.rs
@@ -7,6 +7,7 @@ pub struct TreeNode {
 }
 
 impl TreeNode {
+  #[allow(dead_code)]
   #[inline]
   pub fn new(val: i32) -> Self {
     TreeNode {
@@ -16,8 +17,32 @@ impl TreeNode {
     }
   }
 }
+
 use std::cell::RefCell;
+use std::cmp;
 use std::rc::Rc;
+
+pub struct Solution {}
+
 impl Solution {
-  pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {}
+  #[allow(dead_code)]
+  pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    fn f(node: Option<&Rc<RefCell<TreeNode>>>) -> i32 {
+      if node.is_none() {
+        return 0;
+      }
+
+      match (
+        node.unwrap().borrow().left.as_ref(),
+        node.unwrap().borrow().right.as_ref(),
+      ) {
+        (None, None) => 1,
+        (None, Some(right)) => f(Some(right)),
+        (Some(left), None) => f(Some(left)),
+        (Some(left), Some(right)) => cmp::max(f(Some(left)), f(Some(right))) + 1,
+      }
+    }
+
+    f(root.as_ref())
+  }
 }
